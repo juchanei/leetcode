07e383edfdd265e0ebee1fd29af7067b759cfc29
LEE JUCHAN <juchanei@naver.com>
2019-11-15 22:59:32 +0900

    refactor problem 49

--- medium/49_group_anagrams.js
+++ medium/49_group_anagrams.js
@@ -3,25 +3,32 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-const strToArray = str => {
-    return Array(str.length).fill().map((_, i) => str[i])
+const _ = list => {
+    const l = [...list]
+
+    return {
+        map: fn => _(l.map(fn)),
+        groupBy: fn => {
+            const dict = l.reduce((acc, cur) => {
+                const value = acc[fn(cur)]
+                acc[fn(cur)] = value ? [...value, cur] : [cur]
+                return acc
+            }, {})
+        
+            const ret = Object.keys(dict).map(key => dict[key])
+            return _(ret)
+        },
+        value: () => l
+    }
 }
 
-const groupBy = (objs, fn) => {
-    const dict = objs.reduce((acc, cur) => {
-        const value = acc[fn(cur)]
-        acc[fn(cur)] = value ? [...value, cur] : [cur]
-        return acc
-    }, {})
-
-    return Object.keys(dict).map(key => dict[key])
-}
-
+const strToArray = str => Array(str.length).fill().map((_, i) => str[i])
 const createId = str => strToArray(str).sort().join('')
 
 var groupAnagrams = function (strs) {
-    const pairs = strs.map(str => ({ id: createId(str), str }))
-
-    return groupBy(pairs, ({ id }) => id)
+    return _(strs)
+        .map(str => ({ id: createId(str), str }))
+        .groupBy(({ id }) => id)
         .map(values => values.map(({ str }) => str))
+        .value()
 };
