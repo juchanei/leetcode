59f6327e81d7a2a63b198649d80270635331e82a
LEE JUCHAN <juchanei@naver.com>
2019-12-05 02:09:01 +0900

    add problem 300

--- medium/300_longest_increasing_subsequence.js
+++ medium/300_longest_increasing_subsequence.js
@@ -4,26 +4,28 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    const memo = Array(nums.length).fill(null)
-    const find = index => {
-        const memoized = memo[index]
+    if (nums.length === 0) return 0
+
+    const memo = Array(nums.length).fill().map(_ => Array(nums.length).fill(null))
+    const find = (prev, curr) => {
+        if (curr === nums.length) return 0
+
+        const memoized = memo[prev][curr]
         if (null !== memoized) return memoized
 
-        let ret = 1
-        for (let next = index + 1; next < nums.length; next++) {
-            if (nums[index] < nums[next]) {
-                ret = Math.max(ret, find(next) + 1)
-                if (nums[index] + 1 === nums[next]) break;
-            }
+        let ret = 0
+        if (nums[prev] < nums[curr]) {
+            ret = Math.max(ret, find(curr, curr + 1) + 1)
         }
+        ret = Math.max(ret, find(prev, curr + 1))
         
-        memo[index] = ret
+        memo[prev][curr] = ret
         return ret
     }
 
     let ret = 0
     for (let i = 0; i < nums.length; i++) {
-        ret = Math.max(ret, find(i))
+        ret = Math.max(ret, find(i, i) + 1)
     }
     return ret
 };
